a7eeeed335e23e486db84e8dc181d9eeecbf8fab
Leonardo Brizolla <leonardobrizollars@gmail.com>
2021-04-07 20:49:38 -0300

    Class 002 - Restarting server automatically

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const server = express();
 
 server.get("/", function(req, res) {
-    return res.send("Hi!");
+    return res.send("Hi! How's going?");
 });
 
 server.listen(5000, function() {
