ac42f0850bf1de87eaa1fa2ca6312bb1c8ef2383
Leonardo Brizolla <leonardobrizollars@gmail.com>
2021-04-16 13:08:01 -0300

    Class 011 - Adding featured videos

--- data.js
+++ data.js
@@ -3,36 +3,42 @@ module.exports = [
         id: "DiXbJL3iWVs",
         title: "Espaçamentos e a mágica do CSS Box Model | Masterclass #10",
         duration: "57min",
-        price: "Free"
+        price: "Free",
+        featured: true,
     },
     {
         id: "C8TrGTgvLkc",
         title: "Cache de imagens e regulamento no App do RS/XP",
         duration: "1h",
-        price: "Free"
+        price: "Free",
+        featured: false,
     },
     {
         id: "X2RKRKdqqwM",
         title: "Folder structure for ReactJS/React Native",
         duration: "15min",
-        price: "Free"
+        price: "Free",
+        featured: true,
     },
     {
         id: "rAzHvYnQ8DY",
         title: "Array: Higher Order Functions - Rocketseat",
         duration: "57min",
-        price: "Free"
+        price: "Free",
+        featured: false,
     },
     {
         id: "c7P03kkrEG8",
         title: "Configurando VSCode para Node.js, ReactJS e React Native",
         duration: "22min",
-        price: "Free"
+        price: "Free",
+        featured: true,
     },
     {
         id: "DDm0M_rZLJo",
         title: "Clonando interface do Nubank (+ ANIMAÇÕES) com React Native",
         duration: "55min",
-        price: "Free"
+        price: "Free",
+        featured: false,
     }
 ]
